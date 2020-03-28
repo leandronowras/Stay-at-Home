126bd87a6071590af9b77e20e137f18861de2898
Leandro Abreu <l.leandro_abreu@hotmail.com>
2020-03-28 14:42:06 -0300

    refatoracao do codigo, meter em caixa, add modal e deixar bunito

---
+++ scripts.js
@@ -0,0 +1,6 @@
+function onOff(){
+    document
+        .querySelector("#modal") // vai selecionar o q tem esse id
+        .classList //vai ler tudo q esta escrito
+        .toggle("hide") //vai esconder oq foi lido
+}
