ee4a73250893e1de74fc3653af029e3949f3f55e
Leandro Abreu <l.leandro_abreu@hotmail.com>
2020-03-28 21:46:14 -0300

    fiz a pagina ideias

--- scripts.js
+++ scripts.js
@@ -3,4 +3,15 @@ function onOff(){
         .querySelector("#modal") // vai selecionar o q tem esse id
         .classList //vai ler tudo q esta escrito
         .toggle("hide") //vai esconder oq foi lido
+
+
+    document
+        .querySelector("body") 
+        .classList 
+        .toggle("hideScroll") 
+
+    document
+    .querySelector('#modal')
+    .classList
+    .toggle("addScroll")
 }
